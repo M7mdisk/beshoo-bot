1cfc8d7985bb7ccb7fb07e937d602e5e04598fa8
M7mdisk <M7md.k.isk@gmail.com>
2021-01-20 18:11:05 +0300

    added echo

--- bot.py
+++ bot.py
@@ -9,6 +9,12 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
+def handle_command(command,args):
+    if command.lower() == "echo":
+        return args
+
+
 @client.event
 async def on_ready():
     for guild in client.guilds:
@@ -18,6 +24,18 @@ async def on_ready():
     print(
         f'{client.user} has connected to the following Guild:\n'
         f"{guild.name}(id: {guild.id})"
-        )
+    )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content[0] == '!':
+        command,args = message.content[1:].split(" ",1)
+        print(command,args)
+        response = handle_command(command,args)
+        await message.channel.send(response)
 
 client.run(TOKEN)
