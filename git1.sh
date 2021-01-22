6dbe059d5ee4d1b2f76459308df1bd31c1c3983c
nibras shami <nibrasshami2002@gmail.com>
2021-01-22 19:52:57 +0300

    Changes have been made on Fri Jan 22 19:52:57     2021

--- git1.sh
+++ git1.sh
@@ -3,10 +3,17 @@ cd ~/Documents/Github/min-bot
 
 git add .
 
+echo "--------------------\n"
+
 DATE=$(date)
 
 git commit -m "Changes have been made on $DATE"
 
+echo "--------------------\n"
+
 git push -u
+
+
+echo "--------------------\n"
 
 echo "Done.."
