cdc5fb9c428c612d35d8b20e2fc417bbf73c892f
M7mdisk <M7md.k.isk@gmail.com>
2021-01-31 00:00:18 +0300

    Spam

--- bot3.py
+++ bot3.py
@@ -2,13 +2,13 @@ import os
 import random
 import discord
 from dotenv import load_dotenv
-from discord.ext import commands
 from discord.ext.commands import Bot
-
+from time import sleep
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = discord.Client()
-bot= commands.Bot(command_prefix = '!')
+intents = discord.Intents.default()  
+intents.members = True
+bot= Bot(command_prefix = '!',intents=intents)
 
 
 @bot.event
@@ -18,15 +18,23 @@ async def on_ready():
                           type = discord.ActivityType.watching, 
                           name = ': People coding!!'))
 
-@bot.event
-async def on_message(message):
-    if message.content == "test":
-        await message.channel.send("Testing 1 .. 2 .. 3!")
-
 @bot.command(name = "server")
 async def server_info(context):
     guild = context.guild
-    await context.send(f"server name: {guild.name}\n server size: {len(guild.members)}\n Owner: {guild.owner.display_name}")
+    await context.send(f"server name: {guild.name}\nserver size:{len(guild.members)}\nOwner: {guild.owner.display_name}")
 
+@bot.command()
+async def echo(ctx, *args):
+    await ctx.send(' '.join(args))
 
+@bot.command()
+async def annoy(ctx,user: discord.Member = None,num = 10):
+    if user:
+        if str(user.id) == "787194682354040833":
+            await ctx.send("No hahahahaahaaaaaaaaa get siked")
+        else:
+            for i in range(num):
+                sleep(0.2)
+                await ctx.send(f"{str(user.mention)}, عم اجرب صوتيييي")
+        
 bot.run(TOKEN)
