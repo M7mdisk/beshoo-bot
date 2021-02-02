b8714c419526a14b324f95533ef162a52b2880d5
M7mdisk <M7md.k.isk@gmail.com>
2021-02-02 21:52:55 +0300

    Added BgRemove

---
+++ misc.py
@@ -0,0 +1,116 @@
+import discord
+import requests
+from discord.ext import commands
+from string import printable                                                                                                                                                                           
+import os
+import platform
+OPEN_WEATHER_MAP_KEY = "ea073e04bc85f31dab1408ad497f277f"
+class Miscellaneous(commands.Cog):
+
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(name="meme")
+    async def meme(self,ctx):
+        r = requests.get("https://meme-api.herokuapp.com/gimme/memes")
+        data = r.json()
+        e = discord.Embed()
+        e.set_image(url=data["url"])
+        await ctx.send(embed=e)
+
+
+    @commands.command(name="def")
+    async def urban(self,ctx, word):
+        data= requests.get(f"http://api.urbandictionary.com/v0/define?term={word}").json()
+        top = data['list'][0]
+        definition=f"{top['definition'].replace('[','').replace(']','')}"
+        e = discord.Embed(title=f"{word.title()}",
+                description=definition, color=0x00ff00)
+        await ctx.send(embed=e)
+
+    @commands.command(name="advice")
+    async def advice(self,ctx):
+        r = requests.get("https://api.adviceslip.com/advice")
+        data = r.json()
+        await (data["slip"]["advice"])
+
+    @commands.command()
+    async def echo(self,ctx, *args):
+        await ctx.send(' '.join(args))
+
+    @commands.command()
+    async def annoy(self,ctx,user: discord.Member = None,num = 10):
+        if user:
+            if user.id == 787194682354040833:
+                await ctx.send("No hahahahaahaaaaaaaaa get siked")
+            else:
+                for _ in range(num):
+                    await ctx.send(f"{str(user.mention)}, عم اجرب صوتيييي")
+
+    @commands.command(name = "server")
+    async def server_info(self,ctx):
+        guild = ctx.guild
+        await ctx.send(f"server name: {guild.name}\nserver size:{len(guild.members)}\nOwner: {guild.owner.display_name}")
+
+
+    @commands.command(name="weather",aliases=["طقس"], )
+    async def weather(self,ctx,city,lang ='en'):
+        if not not bool(set(city) - set(printable)) :
+            lang = "ar"
+        msg = await ctx.send("Measuring...")    
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPEN_WEATHER_MAP_KEY}&units=metric&lang={lang}"
+        data = requests.get(url).json()
+        if data['cod'] != 200:
+            embed=discord.Embed(color=0xff0000)
+            embed.add_field(name=f"Error!", value=f"We could not find that city!", inline=False)
+            return await msg.edit(content="",embed=embed)   
+        city = data["name"]
+        temp = data["main"]["temp"]
+        feels_like = data["main"]["feels_like"]
+        desc = data["weather"][0]["description"]
+        icon = data["weather"][0]["icon"]
+        country = data["sys"]["country"].lower()
+        if lang == "ar": 
+            embed=discord.Embed(color=0xffffff)
+            embed.add_field(name=f":flag_{country}: {city}", value=f"{temp}° س", inline=False)
+            embed.add_field(name="كأنها", value=f"{feels_like}° س", inline=True)
+            embed.add_field(name="الوصف", value=desc, inline=True)
+            embed.set_thumbnail(url=f"http://openweathermap.org/img/wn/{icon}@2x.png")
+            return await ctx.send(embed=embed)
+
+        embed=discord.Embed(color=0xffffff)
+        embed.add_field(name=f":flag_{country}: {city}", value=f"{temp}° c", inline=False)
+        embed.add_field(name="Description", value=desc, inline=True)
+        embed.add_field(name="Feels Like", value=f"{feels_like}° c", inline=True)
+        embed.set_thumbnail(url=f"http://openweathermap.org/img/wn/{icon}@2x.png")
+        
+        return await msg.edit(content="",embed=embed)
+
+
+    @commands.command()
+    async def ping(self,ctx):
+        if platform.system() == "Linux":
+            stream = os.popen("vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*'")
+            temp = stream.read()
+        embed=discord.Embed(title="Pong!", color=0x00ff1e)
+        embed.add_field(name="Latency", value=f"{round(self.bot.latency,1)}ms", inline=False)
+        if platform.system() == "Linux":
+            embed.add_field(name="Temperature", value=f"{temp[:-1]}°c", inline=True)
+        await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def bgremove(self,ctx):
+        url = "https://background-removal.p.rapidapi.com/remove"
+        # pic_ext = ['.jpg','.png','.jpeg']
+        if not ctx.message.attachments :
+            return await ctx.send("Attach an image!")
+        payload = f"image_url={ctx.message.attachments[0].url}"
+        headers = {
+            'content-type': "application/x-www-form-urlencoded",
+            'x-rapidapi-key': "157b8ea155msh5a9d9520fc875b8p1d5754jsn1f29da0cc7a7",
+            'x-rapidapi-host': "background-removal.p.rapidapi.com"
+            }
+        r = requests.request("POST", url, data=payload,headers=headers)
+        data = r.json()
+        print(data)
+        await ctx.send(data["response"]["image_url"])
--- run.py
+++ run.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
 from inspect import ArgSpec
 import os
-import platform
 import random
 from tokenize import Name
 import youtube_dl
 from youtube_dl import YoutubeDL
 from requests import get
-from string import printable                                                                                                                                                                           
 import discord
-from discord import FFmpegPCMAudio
-from discord import utils
-from discord import client
-from discord import channel
-from discord import message
+from discord import FFmpegPCMAudio, utils, client, channel, message
 from discord.channel import VoiceChannel
 from discord.ext.commands.core import Command, command
 from dotenv import load_dotenv
@@ -22,12 +16,13 @@ from time import sleep
 import requests
 from ttt import TicTacToe
 from admin import Administration
+from misc import Miscellaneous
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.members = True
 bot= Bot(command_prefix = '!',intents=intents)
-OPEN_WEATHER_MAP_KEY = "ea073e04bc85f31dab1408ad497f277f"
+
 
 @bot.event
 async def on_ready():
@@ -36,56 +31,11 @@ async def on_ready():
                           type = discord.ActivityType.watching, 
                           name = ': People coding!!'))
 
-@bot.command(name = "server")
-async def server_info(context):
-    guild = context.guild
-    await context.send(f"server name: {guild.name}\nserver size:{len(guild.members)}\nOwner: {guild.owner.display_name}")
-
 @bot.event
 async def on_message2(message):
     if message.content == "test":
         await message.channel.send("Testing 1 .. 2 .. 3!")
 
-@bot.command()
-async def echo(ctx, *args):
-    await ctx.send(' '.join(args))
-
-@bot.command()
-async def annoy(ctx,user: discord.Member = None,num = 10):
-    if user:
-        if user.id == 787194682354040833:
-            await ctx.send("No hahahahaahaaaaaaaaa get siked")
-        else:
-            for _ in range(num):
-                sleep(0.2)
-                await ctx.send(f"{str(user.mention)}, عم اجرب صوتيييي")
-
-
-@bot.command(name="advice")
-async def advice(ctx):
-    r = requests.get("https://api.adviceslip.com/advice")
-    data = r.json()
-    await (data["slip"]["advice"])
-
-
-@bot.command(name="def")
-async def urban(ctx, word):
-    data= requests.get(f"http://api.urbandictionary.com/v0/define?term={word}").json()
-    top = data['list'][0]
-    definition=f"{top['definition'].replace('[','').replace(']','')}"
-    e = discord.Embed(title=f"{word.title()}",
-            description=definition, color=0x00ff00)
-    await ctx.send(embed=e)
-
-
-
-@bot.command(name="meme")
-async def meme(ctx):
-    r = requests.get("https://meme-api.herokuapp.com/gimme/memes")
-    data = r.json()
-    e = discord.Embed()
-    e.set_image(url=data["url"])
-    await ctx.send(embed=e)
 
 ### new:
 @bot.command(name = "join")
@@ -147,58 +97,8 @@ async def leave(ctx):
     await ctx.send("I left the Voice channel!")
 
 
-@bot.command(name = "editme")
-async def edit_me(ctx):
-  message = await ctx.send("hello")
-  await message.edit(content="bye!")
-
-
-@bot.command(name="weather",aliases=["طقس"], )
-async def weather(ctx,city,lang ='en'):
-    if not not bool(set(city) - set(printable)) :
-        lang = "ar"
-    msg = await ctx.send("Measuring...")    
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPEN_WEATHER_MAP_KEY}&units=metric&lang={lang}"
-    data = requests.get(url).json()
-    if data['cod'] != 200:
-        embed=discord.Embed(color=0xff0000)
-        embed.add_field(name=f"Error!", value=f"We could not find that city!", inline=False)
-        return await msg.edit(content="",embed=embed)   
-    city = data["name"]
-    temp = data["main"]["temp"]
-    feels_like = data["main"]["feels_like"]
-    desc = data["weather"][0]["description"]
-    icon = data["weather"][0]["icon"]
-    country = data["sys"]["country"].lower()
-    if lang == "ar": 
-        embed=discord.Embed(color=0xffffff)
-        embed.add_field(name=f":flag_{country}: {city}", value=f"{temp}° س", inline=False)
-        embed.add_field(name="كأنها", value=f"{feels_like}° س", inline=True)
-        embed.add_field(name="الوصف", value=desc, inline=True)
-        embed.set_thumbnail(url=f"http://openweathermap.org/img/wn/{icon}@2x.png")
-        return await ctx.send(embed=embed)
-
-    embed=discord.Embed(color=0xffffff)
-    embed.add_field(name=f":flag_{country}: {city}", value=f"{temp}° c", inline=False)
-    embed.add_field(name="Description", value=desc, inline=True)
-    embed.add_field(name="Feels Like", value=f"{feels_like}° c", inline=True)
-    embed.set_thumbnail(url=f"http://openweathermap.org/img/wn/{icon}@2x.png")
-    
-    return await msg.edit(content="",embed=embed)
-
-
-
-@bot.command()
-async def ping(ctx):
-    stream = os.popen("vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*'")
-    temp = stream.read()
-    embed=discord.Embed(title="Pong!", color=0x00ff1e)
-    embed.add_field(name="Latency", value=f"{round(bot.latency,1)}ms", inline=False)
-    if platform.system() == "Linux":
-        embed.add_field(name="Temperature", value=f"{temp[:-1]}°c", inline=True)
-    await ctx.send(embed=embed)
-
 bot.add_cog(TicTacToe(bot))
 bot.add_cog(Administration(bot))
+bot.add_cog(Miscellaneous(bot))
 
 bot.run(TOKEN)
