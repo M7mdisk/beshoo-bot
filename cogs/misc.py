cff6f74e4e0864c8274f2e43e5332b0de37da5c0
M7mdisk <M7md.k.isk@gmail.com>
2021-02-16 23:46:58 +0300

    Added poll command

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,4 +1,6 @@
 import os
+import time
+import datetime
 from dotenv import load_dotenv
 import discord
 from discord import colour
@@ -6,6 +8,7 @@ import requests
 from discord.ext import commands
 from string import printable
 import platform
+import asyncio
 
 load_dotenv()
 OPEN_WEATHER_MAP_KEY = os.getenv('OPEN_WEATHER_MAP_KEY')
@@ -115,7 +118,7 @@ class Miscellaneous(commands.Cog):
     async def ping(self,ctx):
         '''Check the bot's ping'''
         embed=discord.Embed(title="Pong!", color=0x00ff1e)
-        embed.add_field(name="Latency", value=f"{round(self.bot.latency,1)}ms", inline=False)
+        embed.add_field(name="Latency", value=f"{round(self.bot.latency,4)}ms", inline=False)
         await ctx.send(embed=embed)
 
 
@@ -133,8 +136,45 @@ class Miscellaneous(commands.Cog):
         await ctx.send(embed=embed)
     
     @commands.command()
+    # @commands.guild_only()
+    async def poll(self,ctx,question,minutes = 1):
+        minutes_added = datetime.timedelta(minutes = minutes)
+        ends_at = datetime.datetime.now() + minutes_added
+        embed=discord.Embed(title=f"{question}", description=f'(ends at {ends_at.strftime("%H:%M")})', color=0xd2ba1e)
+        embed.set_footer(text=f"requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+        msg = await ctx.send(embed=embed)
+        await msg.add_reaction("✅")
+        await msg.add_reaction("❌")
+        msg = await msg.channel.fetch_message(msg.id)
+        t_end = time.time() + 60 * minutes # create a timeout timer for how long user can switch page
+        while time.time() < t_end: # while time is still ok
+            await asyncio.sleep(1)
+        msg = await msg.channel.fetch_message(msg.id)
+        reactions = msg.reactions
+        yes,no =0,0 
+        for reaction in reactions:
+            if str(reaction.emoji) == '✅':
+                yes = reaction.count
+            elif str(reaction.emoji) == '❌':
+                no = reaction.count
+        if yes == no:
+            embed=discord.Embed(color=0x0000ff)
+            embed.add_field(name=f"Result for '{question}'", value="it is a tie!", inline=False)
+            return await ctx.send(embed=embed)
+        elif yes > no:
+            embed=discord.Embed(color=0x00ff00)
+            embed.add_field(name=f"✅ Result for '{question}'", value="The people voted yes!", inline=False)
+            return await ctx.send(embed=embed)
+        else:
+            embed=discord.Embed(color=0xff0000)
+            embed.add_field(name=f"❌ Result for '{question}'", value="The people voted no!", inline=False)
+            return await ctx.send(embed=embed)
+
+
+
+
+    @commands.command()
     async def slap(self, ctx, members: commands.Greedy[discord.Member], *, reason='no reason'):
         ''' Slap any user you want to punish :3 '''
         slapped = ", ".join(x.name for x in members)
         await ctx.send('{} just got slapped for {}'.format(slapped, reason))
-
